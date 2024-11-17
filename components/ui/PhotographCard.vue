23614642df94b0fc59d4a4b90bc39ff25fa20516
jordanolv <olivier.jordan21@gmail.com>
2024-11-17 23:52:28 +0100

    add props to PhotographCard.vue

--- components/ui/PhotographCard.vue
+++ components/ui/PhotographCard.vue
@@ -4,8 +4,8 @@
       <div class="w-full h-32 bg-gray-200 rounded"></div>
       <span class="absolute top-2 right-2 bg-primary-mid text-white text-xs px-2 py-1 rounded">??</span>
     </div>
-    <div class="text-lg font-semibold">Name</div>
-    <p class="text-sm text-gray-500">Short desc</p>
+    <div class="text-lg font-semibold">{{ photograph.firstName }} {{ photograph.lastName }}</div>
+    <p class="text-sm text-gray-500">{{ photograph.description }}</p>
     <div class="mt-4 flex justify-between items-center">
       <div>
         <p class="text-primary-mid font-bold">$200.25</p>
@@ -16,6 +16,15 @@
 </template>
 
 <script setup lang="ts">
+const props = defineProps({
+  photograph: {
+    type: Object,
+    required: true,
+    default: {},
+  },
+});
+
+const { photograph } = props;
 </script>
 
 <style scoped>
