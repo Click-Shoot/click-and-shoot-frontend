3bbcca19e810ac02dc2c92dda41178128e7f25c2
Jordan <olivier.jordan21@gmail.com>
2024-11-18 23:48:41 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto p-4 space-y-8">
+  <div class="container mx-auto p-4 space-y-8 mt-8">
     <AnnonceBanner />
     <CategoryCarousel />
     <RecommendSection />
